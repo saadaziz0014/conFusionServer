df0c5c8ccf75e8a8aa8f8bf95338c112b69a96cd
saadaziz <saadaziz0014@gmail.com>
2022-10-29 10:45:36 +0500

    assignment4

--- app.js
+++ app.js
@@ -1,73 +1,48 @@
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
+var favicon = require("serve-favicon");
 var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+var passport = require("passport");
+var authenticate = require("./authenticate");
+var config = require("./config");
+
+var index = require("./routes/index");
+var users = require("./routes/users");
+var dishRouter = require("./routes/dishRouter");
+var leaderRouter = require("./routes/leaderRouter");
+var promoRouter = require("./routes/promoRouter");
+var uploadRouter = require("./routes/uploadRouter");
+var favouriteRouter = require("./routes/favouriteRouter");
+
 const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
+
 const Dishes = require("./models/dishes");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var dishRouter = require("./routes/dishRouter");
-var promoRouter = require("./routes/promotionRouter");
-var leaderRouter = require("./routes/leaderRouter");
+const url = config.mongoUrl;
 
-const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  useMongoClient: true,
 });
 
-function auth(req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
-    return;
+connect.then(
+  (db) => {
+    console.log("Connected to server!");
+  },
+  (err) => {
+    console.log(err);
   }
-  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == "admin" && pass == "password") {
-    next(); // authorized
-  } else {
-    var err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
-  }
-}
+);
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(auth);
-app.use(cookieParser("12345-67890-09876-54321"));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/dishes", dishRouter);
-app.use("/promotions", promoRouter);
-app.use("/leaders", leaderRouter);
-
-app.all("*", (req, res, next) => {
+app.all("*", (req, res, nxt) => {
   if (req.secure) {
-    return next();
+    return nxt();
   } else {
     res.redirect(
       307,
@@ -76,17 +51,65 @@ app.all("*", (req, res, next) => {
   }
 });
 
-connect
-  .then((db) => {
-    console.log("Connected to server");
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    name: "session-id",
+    secret: "Jesus-Loves-w@ch!ra-so-Much!!",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
   })
-  .catch((err) => {
-    console.log(err);
-  });
+);
+// app.use(session({secret: 'SECRET'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/", index);
+app.use("/users", users);
+
+// authentication before resource access
+// function auth(req, res, next) {
+//   // console.log(req.signedCookies);
+//   console.log(req.session);
+
+//   // if(!req.signedCookies.user){
+//   if(!req.user){
+//     // var authHeader = req.headers.authorization;
+//     // if(!authHeader){
+//       var err = new Error('You are not authenticated!');
+//       // res.setHeader('WWW-Authenticate', 'Basic');
+//       err.status = 403;
+//       return next(err);
+//     }
+//     else {
+//       // if(req.signedCookies.user === 'admin'){
+//       next();
+//     }
+// }
+// app.use(auth);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/dishes", dishRouter);
+app.use("/leaders", leaderRouter);
+app.use("/promotions", promoRouter);
+app.use("/imageUpload", uploadRouter);
+app.use("/favorites", favouriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
